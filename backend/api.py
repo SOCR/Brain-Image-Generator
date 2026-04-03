e2af2117a3aef5595a7217be1b23ed698487257f
Simeone Marino <simeonem@gmail.com>
2026-04-03 13:23:35 -0400

    Update api.py
    Changed production allowed origins urls

--- backend/api.py
+++ backend/api.py
@@ -24,9 +24,17 @@ if frontend_url == "*":
 else:
     # Specific origins for production
     allowed_origins = [
-        "http://localhost:3000",  # Local development
-        frontend_url,  # Your Vercel deployment
-    ]
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://brain-image-generator.vercel.app",
+    "https://ai-bot.statisticalcomputing.org",
+]
+    # allowed_origins = [
+    #    "http://localhost:3000",  # Local development
+    #    frontend_url,  # Your Vercel deployment
+    #]
 
 app.add_middleware(
     CORSMiddleware,
